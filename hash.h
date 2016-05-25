a65a1251f9eca8b7fb252b6125ddabc81deddcbb
Joel Walker <joel@walker9.net>
2016-05-24 23:34:48 -0500

    Added Hash

---
+++ hash.h
@@ -0,0 +1,19 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <cstddef>
+
+namespace jw_util
+{
+
+class Hash
+{
+    static constexpr std::size_t combine(std::size_t hash, std::size_t val)
+    {
+        return hash ^ (val + 0x9e3779b9 + (hash << 6) + (hash >> 2));
+    }
+};
+
+}
+
+#endif // HASH_H
