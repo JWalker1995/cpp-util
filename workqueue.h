2d58b9a58267e2f3f3bce3485da2a0bda504d96d
Joel Walker <joel@walker9.net>
2016-06-05 13:49:23 -0500

    Added WorkQueueInsomniac

--- workqueue.h
+++ workqueue.h
@@ -1,140 +1,24 @@
 #ifndef JWUTIL_WORKQUEUE_H
 #define JWUTIL_WORKQUEUE_H
 
-#include <array>
-#include <tuple>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <queue>
-
-#include "libBlobPlanet/util/methodcallback.h"
+#include "workqueuebase.h"
 
 namespace jw_util
 {
 
 template <unsigned int num_threads, typename... ArgTypes>
-class WorkQueue
+class WorkQueue : public WorkQueueBase<WorkQueue<num_threads, ArgTypes...>, num_threads, ArgTypes...>
 {
+    typedef WorkQueueBase<WorkQueue<num_threads, ArgTypes...>, num_threads, ArgTypes...> BaseType;
+    friend BaseType;
+
 public:
-    struct construct_paused_t {};
-    static constexpr construct_paused_t construct_paused {};
-
-    WorkQueue(jw_util::MethodCallback<ArgTypes...> worker)
-        : worker(worker)
-        , running(false)
-    {
-        start();
-    }
-
-    WorkQueue(jw_util::MethodCallback<ArgTypes...> worker, construct_paused_t)
-        : worker(worker)
-        , running(false)
-    {}
-
-    ~WorkQueue()
-    {
-        if (running)
-        {
-            pause();
-        }
-    }
-
-    void push(ArgTypes... args)
-    {
-        assert(running);
-
-        if (num_threads)
-        {
-            {
-                std::lock_guard<std::mutex> lock(mutex);
-                (void) lock;
-                queue.emplace(std::forward<ArgTypes>(args)...);
-            }
-            conditional_variable.notify_one();
-        }
-        else
-        {
-            worker.call(std::forward<ArgTypes>(args)...);
-        }
-    }
-
-    void start()
-    {
-        assert(!running);
-
-        running = true;
-
-        for (unsigned int i = 0; i < num_threads; i++)
-        {
-            threads[i] = std::thread(&WorkQueue<num_threads, ArgTypes...>::loop, this);
-        }
-    }
-
-    void pause()
-    {
-        assert(running);
-
-        running = false;
-
-        if (num_threads)
-        {
-            conditional_variable.notify_all();
-        }
-
-        for (unsigned int i = 0; i < num_threads; i++)
-        {
-            threads[i].join();
-        }
-    }
+    using BaseType::WorkQueueBase;
 
 private:
-    typedef std::tuple<typename std::remove_reference<ArgTypes>::type...> TupleType;
-
-    const jw_util::MethodCallback<ArgTypes...> worker;
-
-    std::array<std::thread, num_threads> threads;
-
-    std::mutex mutex;
-    std::condition_variable conditional_variable;
-
-    std::queue<TupleType> queue;
-
-    bool running;
-
-    void loop()
+    void wait(std::unique_lock<std::mutex> &lock)
     {
-        assert(num_threads);
-
-        std::unique_lock<std::mutex> lock(mutex);
-        while (true)
-        {
-            if (queue.empty())
-            {
-                if (!running) {break;}
-                conditional_variable.wait(lock);
-            }
-            else
-            {
-                TupleType args = std::move(queue.front());
-                queue.pop();
-
-                lock.unlock();
-                dispatch(std::move(args));
-                lock.lock();
-            }
-        }
-    }
-
-    void dispatch(TupleType &&args)
-    {
-        call(std::forward<TupleType>(args), std::index_sequence_for<ArgTypes...>{});
-    }
-
-    template<std::size_t... Indices>
-    void call(TupleType &&args, std::index_sequence<Indices...>)
-    {
-        worker.call(std::forward<ArgTypes>(std::get<Indices>(std::forward<TupleType>(args)))...);
+        BaseType::conditional_variable.wait(lock);
     }
 };
 
---
+++ workqueueinsomniac.h
@@ -0,0 +1,52 @@
+#ifndef JWUTIL_WORKQUEUEINSOMNIAC_H
+#define JWUTIL_WORKQUEUEINSOMNIAC_H
+
+#include <chrono>
+
+#include "workqueuebase.h"
+
+namespace jw_util
+{
+
+template <unsigned int num_threads, typename... ArgTypes>
+class WorkQueueInsomniac : public WorkQueueBase<WorkQueueInsomniac<num_threads, ArgTypes...>, num_threads, ArgTypes...>
+{
+    typedef WorkQueueBase<WorkQueueInsomniac<num_threads, ArgTypes...>, num_threads, ArgTypes...> BaseType;
+    friend BaseType;
+
+public:
+    using BaseType::WorkQueueBase;
+
+    void set_wakeup_worker(jw_util::MethodCallback<> worker)
+    {
+        wakeup_worker = worker;
+    }
+
+    template <typename DurationRep, typename DurationPeriod>
+    void set_wakeup_interval(std::chrono::duration<DurationRep, DurationPeriod> duration)
+    {
+        next_wakeup = std::chrono::steady_clock::now();
+        wakeup_interval = std::chrono::duration_cast<std::chrono::steady_clock::duration>(duration);
+    }
+
+private:
+    jw_util::MethodCallback<> wakeup_worker;
+
+    std::chrono::steady_clock::time_point next_wakeup = std::chrono::steady_clock::time_point::max();
+    std::chrono::steady_clock::duration wakeup_interval = std::chrono::steady_clock::duration::max();
+
+    void wait(std::unique_lock<std::mutex> &lock)
+    {
+        std::cv_status status = BaseType::conditional_variable.wait_until(lock, next_wakeup);
+
+        if (status == std::cv_status::timeout)
+        {
+            next_wakeup += wakeup_interval;
+            wakeup_worker.call();
+        }
+    }
+};
+
+}
+
+#endif // JWUTIL_WORKQUEUEINSOMNIAC_H
