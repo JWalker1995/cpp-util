66171c468b0282f3c621f0faf8f8e68a164bf8f6
Joel Walker <joel.walker@maryville.com>
2017-04-13 11:44:49 -0500

    Made to_string method static

--- to_string.h
+++ to_string.h
@@ -6,16 +6,16 @@
 namespace jw_util {
 
 template<typename ArgType>
-std::string to_string(const ArgType &arg) {
+static std::string to_string(const ArgType &arg) {
     return std::to_string(arg);
 }
-std::string to_string(const char *arg) {
+static std::string to_string(const char *arg) {
     return arg;
 }
-std::string to_string(const std::string &arg) {
+static std::string to_string(const std::string &arg) {
     return arg;
 }
-std::string to_string(char *const *arg) {
+static std::string to_string(char *const *arg) {
     std::string res;
 
     if (*arg == 0) {return res;}
@@ -26,7 +26,7 @@ std::string to_string(char *const *arg) {
         res += ", ";
     }
 }
-std::string to_string(char **arg) {
+static std::string to_string(char **arg) {
     return to_string(const_cast<char *const *>(arg));
 }
 
