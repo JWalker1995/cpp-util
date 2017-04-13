c1cbe53b998a603c0e3b67ddf14700eb17aed610
Joel Walker <joel.walker@maryville.com>
2017-04-13 13:01:59 -0500

    Made to_string methods inline

--- to_string.h
+++ to_string.h
@@ -6,16 +6,23 @@
 namespace jw_util {
 
 template<typename ArgType>
-static std::string to_string(const ArgType &arg) {
-    return std::to_string(arg);
+inline static std::string to_string(const ArgType &arg) {
+    return typeid(ArgType).name();
 }
-static std::string to_string(const char *arg) {
-    return arg;
-}
-static std::string to_string(const std::string &arg) {
-    return arg;
-}
-static std::string to_string(char *const *arg) {
+
+inline static std::string to_string(int arg) {return std::to_string(arg);}
+inline static std::string to_string(long arg) {return std::to_string(arg);}
+inline static std::string to_string(long long arg) {return std::to_string(arg);}
+inline static std::string to_string(unsigned arg) {return std::to_string(arg);}
+inline static std::string to_string(unsigned long arg) {return std::to_string(arg);}
+inline static std::string to_string(unsigned long long arg) {return std::to_string(arg);}
+inline static std::string to_string(float arg) {return std::to_string(arg);}
+inline static std::string to_string(double arg) {return std::to_string(arg);}
+inline static std::string to_string(long double arg) {return std::to_string(arg);}
+inline static std::string to_string(const char *arg) {return arg;}
+inline static std::string to_string(const std::string &arg) {return arg;}
+
+inline static std::string to_string(char *const *arg) {
     std::string res;
 
     if (*arg == 0) {return res;}
@@ -26,7 +33,7 @@ static std::string to_string(char *const *arg) {
         res += ", ";
     }
 }
-static std::string to_string(char **arg) {
+inline static std::string to_string(char **arg) {
     return to_string(const_cast<char *const *>(arg));
 }
 
