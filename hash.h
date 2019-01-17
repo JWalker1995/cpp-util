b24b37c153b3cf268b459aa89c7f5b074d2777d8
Joel Walker <joelwalker1995@gmail.com>
2019-01-16 22:21:11 -0600

    Generalized Hash class

--- hash.h
+++ hash.h
@@ -6,12 +6,11 @@
 namespace jw_util
 {
 
-class Hash
-{
+template <typename HashType = std::size_t>
+class Hash {
 public:
-    static constexpr std::size_t combine(std::size_t hash, std::size_t val)
-    {
-        return hash ^ (val + 0x9e3779b9 + (hash << 6) + (hash >> 2));
+    static constexpr HashType combine(HashType hash, HashType val) {
+        return hash ^ (val + static_cast<HashType>(0x9e3779b97f4a7c15u) + (hash << 6) + (hash >> 2));
     }
 };
 
